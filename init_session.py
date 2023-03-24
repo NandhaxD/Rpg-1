9368409f0ef34c61a2bf0463400ed4c436d2df2d
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2023-03-24 17:19:13 +0530

    Delete init_session.py

--- init_session.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite+pysqlite:///gametools.db', echo=True)
-session = Session(engine)
-Base = declarative_base()
-Base.metadata.create_all(engine)
