ee694f8877508df497611985f6588810bb8d2b87
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2023-03-24 17:24:54 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,43 +2,43 @@ import asyncio
 import motor.motor_asyncio
 import bot
 from classes import Persons, Locations
+from config import MONGO_DB
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
+db = client.Rpg
 
 
-client = motor.motor_asyncio.AsyncIOMotorClient()
-db = client.game
-
-
-async def go_loc(username, loc_id, call):
+async def go_loc(username, loc_id, message):
     user = await db.persons.find_one({"Nickname": username})
     user["LocationID"] = loc_id
     await db.persons.replace_one({"_id": user["_id"]}, user)
     if loc_id > 0:
         cur_place = (await db.locations.find_one({"LocationID": loc_id}))["LocationType"]
         if cur_place == 'town':
-            await get_town(call)
+            await get_town(message)
         elif cur_place == 'dungeon':
-            await get_dungeon(call)
+            await get_dungeon(message)
 
 
-async def get_town(call):
-    cur_town_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+async def get_town(message):
+    cur_town_id = (await db.persons.find_one({"Nickname": message.from_user.username}))["LocationID"]
     cur_town = (await db.locations.find_one({"LocationID": cur_town_id}))["LocationName"]
-    player = await db.persons.find_one({"Nickname": call.from_user.username})
+    player = await db.persons.find_one({"Nickname": message.from_user.username})
     player["CurHP"] = player["HP"]
-    await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+    await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                 text=f"You Are In The City: 🏰 *{cur_town}*", reply_markup=bot.town_markup, parse_mode="Markdown")
 
 
-async def get_dungeon(call):
-    cur_dungeon_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+async def get_dungeon(message):
+    cur_dungeon_id = (await db.persons.find_one({"Nickname": message.from_user.username}))["LocationID"]
     cur_dungeon = (await db.locations.find_one({"LocationID": cur_dungeon_id}))["LocationName"]
-    await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+    await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                 text=f"You Are In The Dungeon: ⛰️ *{cur_dungeon}*", reply_markup=bot.dungeon_gate_markup,
                                 parse_mode="Markdown")
 
 
-async def get_map(call):
-    cur_town_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+async def get_map(message):
+    cur_town_id = (await db.persons.find_one({"Nickname": message.from_user.username}))["LocationID"]
     cur_town_x = (await db.locations.find_one({"LocationID": cur_town_id}))["XCoord"]
     cur_town_y = (await db.locations.find_one({"LocationID": cur_town_id}))["YCoord"]
     destinations = db.locations.find()
@@ -48,19 +48,19 @@ async def get_map(call):
         if 0 < dist <= 10:
             text += f"{el['LocationName']} - {dist} Km 🛣️\n\n"
     if cur_town_id == 1:
-        await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+        await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                     text=text, reply_markup=bot.choose_location_1_markup,
                                     parse_mode="Markdown")
     elif cur_town_id == 2:
-        await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+        await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                     text=text, reply_markup=bot.choose_location_2_markup,
                                     parse_mode="Markdown")
     elif cur_town_id == 3:
-        await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+        await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                     text=text, reply_markup=bot.choose_location_3_markup,
                                     parse_mode="Markdown")
     elif cur_town_id == 4:
-        await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+        await bot.bot.edit_message_text(chat_id=message.chat.id, message_id=message.id,
                                     text=text, reply_markup=bot.choose_location_4_markup,
                                     parse_mode="Markdown")
 
