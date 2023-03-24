dd1ff6a65ded19e273e16aab2f2940c0c0be6211
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2023-03-24 18:23:36 +0530

    Update __int__.py

--- Sylvie/__int__.py
+++ Sylvie/__int__.py
@@ -1,1 +1,13 @@
+from pyrogram import *
+from motor.motor_asyncio import AsyncIOMotorClient as async_mongo
+from config import *
 
+app = Client(
+    'Slave',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+app.start()
+async_mongo_client = async_mongo(MONGO_URI)
+db = async_mongo_client.Rpg
