19c85792f23fb299881ec30b179dd87e7e70b753
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2023-03-24 17:17:59 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,16 +1,19 @@
-from sqlalchemy import select
+import asyncio
+import motor.motor_asyncio
 import bot
-from init_session import session
 from classes import Persons, Locations
 
 
+client = motor.motor_asyncio.AsyncIOMotorClient()
+db = client.game
+
+
 async def go_loc(username, loc_id, call):
-    stmt = select(Persons).where(Persons.Nickname == username)
-    user = session.scalars(stmt).one()
-    user.LocationID = loc_id
-    session.commit()
+    user = await db.persons.find_one({"Nickname": username})
+    user["LocationID"] = loc_id
+    await db.persons.replace_one({"_id": user["_id"]}, user)
     if loc_id > 0:
-        cur_place = session.execute(select(Locations.LocationType).where(Locations.LocationID == loc_id)).scalar()
+        cur_place = (await db.locations.find_one({"LocationID": loc_id}))["LocationType"]
         if cur_place == 'town':
             await get_town(call)
         elif cur_place == 'dungeon':
@@ -18,36 +21,32 @@ async def go_loc(username, loc_id, call):
 
 
 async def get_town(call):
-    cur_town_id = session.execute(
-        select(Persons.LocationID).where(Persons.Nickname == call.from_user.username)).scalar()
-    cur_town = session.execute(select(Locations.LocationName).where(Locations.LocationID == cur_town_id)).scalar()
-    stmt = select(Persons).where(Persons.Nickname == call.from_user.username)
-    player = session.scalars(stmt).one()
-    player.CurHP = player.HP
+    cur_town_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+    cur_town = (await db.locations.find_one({"LocationID": cur_town_id}))["LocationName"]
+    player = await db.persons.find_one({"Nickname": call.from_user.username})
+    player["CurHP"] = player["HP"]
     await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
                                 text=f"You Are In The City: 🏰 *{cur_town}*", reply_markup=bot.town_markup, parse_mode="Markdown")
 
 
 async def get_dungeon(call):
-    cur_dungeon_id = session.execute(
-        select(Persons.LocationID).where(Persons.Nickname == call.from_user.username)).scalar()
-    cur_dungeon = session.execute(select(Locations.LocationName).where(Locations.LocationID == cur_dungeon_id)).scalar()
+    cur_dungeon_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+    cur_dungeon = (await db.locations.find_one({"LocationID": cur_dungeon_id}))["LocationName"]
     await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
                                 text=f"You Are In The Dungeon: ⛰️ *{cur_dungeon}*", reply_markup=bot.dungeon_gate_markup,
                                 parse_mode="Markdown")
 
 
 async def get_map(call):
-    cur_town_id = session.execute(
-        select(Persons.LocationID).where(Persons.Nickname == call.from_user.username)).scalar()
-    cur_town_x = session.execute(select(Locations.XCoord).where(Locations.LocationID == cur_town_id)).scalar()
-    cur_town_y = session.execute(select(Locations.YCoord).where(Locations.LocationID == cur_town_id)).scalar()
-    destinations = session.execute(select(Locations))
+    cur_town_id = (await db.persons.find_one({"Nickname": call.from_user.username}))["LocationID"]
+    cur_town_x = (await db.locations.find_one({"LocationID": cur_town_id}))["XCoord"]
+    cur_town_y = (await db.locations.find_one({"LocationID": cur_town_id}))["YCoord"]
+    destinations = db.locations.find()
     text = '*Available Locations:*\n\n'
-    for el in destinations:
-        dist = round(count_distance(cur_town_x, cur_town_y, el.Locations.XCoord, el.Locations.YCoord))
+    async for el in destinations:
+        dist = round(count_distance(cur_town_x, cur_town_y, el["XCoord"], el["YCoord"]))
         if 0 < dist <= 10:
-            text += f"{el.Locations.LocationName} - {dist} Km 🛣️\n\n"
+            text += f"{el['LocationName']} - {dist} Km 🛣️\n\n"
     if cur_town_id == 1:
         await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
                                     text=text, reply_markup=bot.choose_location_1_markup,
@@ -63,8 +62,6 @@ async def get_map(call):
     elif cur_town_id == 4:
         await bot.bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
                                     text=text, reply_markup=bot.choose_location_4_markup,
-                                    parse_mode="Markdown")
-
 
 def count_distance(x1, y1, x2, y2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2)
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2))
