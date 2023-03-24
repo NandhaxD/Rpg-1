e16a17e1544892219aef13029e8755bd72c08551
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2023-03-24 18:27:47 +0530

    Update __init__.py

--- Sylvie/Database/__init__.py
+++ Sylvie/Database/__init__.py
@@ -1,4 +1,6 @@
 from Database.classes import *
+from Database.functions import *
+from Database.db import *
 
 # Items
 common_sword = Items(Name="Regular Sword", Cost=10, CostToSale=8, ItemType='weapon', HP=0, Mana=0, Attack=2,
