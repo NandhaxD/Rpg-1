cd61a6cc54dcb58d79b557167f56889fb2dbe866
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 20:53:33 +0530

    Update __init__.py

--- Aasf/__init__.py
+++ Aasf/__init__.py
@@ -5,10 +5,10 @@ from motor.motor_asyncio import AsyncIOMotorClient as async_mongo
 
 
 app = Client(
-    "Aasf",
-    "14676558",
-    "b3c4bc0ba6a4fc123f4d748a8cc39981",
-    "6690815586:AAFdkuqgTo2EHlkXlid9HBC2hnsW0flVsDU"
+    name="Aasf",
+    api_id="14676558",
+    api_hash="b3c4bc0ba6a4fc123f4d748a8cc39981",
+    bot_token="5617659434:AAFPGDeIl7JNHEHxx3t0s1DBzt2rQvc0DpY"
 )
 app.start()
 async_mongo_client = async_mongo("mongodb+srv://Aasf:Aasf@aasf.ere7cca.mongodb.net/?retryWrites=true&w=majority&appName=Aasf")
