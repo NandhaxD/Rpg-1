b7b366fc181bd18d4d627eb352ba7a7867190318
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 15:08:02 +0530

    Update __int__.py

--- Sylvie/__int__.py
+++ Sylvie/__int__.py
@@ -6,9 +6,9 @@ from config import *
 
 app = Client(
     "Sylvie",
-    api_id="",
-    api_hash="",
-    bot_token=""
+    api_id="14676558",
+    api_hash="b3c4bc0ba6a4fc123f4d748a8cc39981",
+    bot_token="6690815586:AAGsAZ4n8fJX6t_jgdJmvNaKErpvEp5VXxs"
 )
 app.start()
 async_mongo_client = async_mongo("mongodb+srv://Aasf:<password>@aasf.ere7cca.mongodb.net/?retryWrites=true&w=majority&appName=Aasf")
