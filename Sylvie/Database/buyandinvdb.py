e522adc7362b4698f63e5b2a3b127dc57b4fb3c1
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 11:12:00 +0530

    Update buyandinvdb.py

--- Sylvie/Database/buyandinvdb.py
+++ Sylvie/Database/buyandinvdb.py
@@ -1,7 +1,7 @@
 from Sylvie import *
 
 async def add_item(user_id, item_id):
-    doc = {'user_id': user_id, 'item_id': item_id, 'quantity': 1}
+    doc = {'player': user_id, 'item_id': item_id, 'quantity': 1}
     result = await db.inventory.find_one(doc)
     if not result:
         result = await db.inventory.insert_one(doc)
@@ -9,13 +9,13 @@ async def add_item(user_id, item_id):
         return None
 
 async def increase_item(user_id, item_id):
-    filter = {'user_id': user_id, 'item_id': item_id}
+    filter = {'player': user_id, 'item_id': item_id}
     update = {'$inc': {'quantity': 1}}
     result = await db.inventory.update_one(filter, update)
     return None
 
 async def get_item(user_id, item_id):
-    filter = {'user_id': user_id, 'item_id': item_id}
+    filter = {'player': user_id, 'item_id': item_id}
     item = await db.inventory.find_one(filter)
     if item:
         return item
@@ -23,7 +23,7 @@ async def get_item(user_id, item_id):
         return False
 
 async def get_inventory(user_id):
-    user_filter = {'user_id': user_id}
+    user_filter = {'player': user_id}
     user_inv = []
     async for doc in db.inventory.find(user_filter):
         user_inv.append(doc)
@@ -31,6 +31,13 @@ async def get_inventory(user_id):
         return None
     return user_inv
 
+async def update_inventory(user_id, item_id, inv):
+    inventory = await db.find_one({'player': user_id, 'item_id': item_to_sell})
+    if inventory:
+        await db.inventory.replace_one({'player': user_id, 'item_id': item_id}, inv)
+        return True
+    else:
+        return False
 def get_key(val, my_dict):
     for key, value in my_dict.items():
         if val == value:
