04243b57c69569e987ecbcc83e83b8acc87b7099
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 15:41:02 +0530

    Update __init__.py

--- Sylvie/Database/__init__.py
+++ Sylvie/Database/__init__.py
@@ -1,3 +1,10 @@
+from .battledb import *
+from .buyandinvdb import *
+from .functions import *
+from .locationdb import *
+from .playerdb import *
+from Sylvie import *
+
 locations = {
     1: {"location_name": "Adventurer's Guild", "x_coord": 0, "y_coord": 0, "location_type": "town", "location_id": 1, "req_level": 1}
 }
