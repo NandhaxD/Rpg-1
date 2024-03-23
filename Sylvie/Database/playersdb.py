3eb6377101a3b6cf308ad7b968ffe0492821d9c7
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 09:58:47 +0530

    Create playersdb.py

---
+++ Sylvie/Database/playersdb.py
@@ -0,0 +1,24 @@
+from Sylvie.Database import *
+
+async def get_player(user_id: int):
+    player = await db.player.find_one({"player": user_id})
+    if player:
+        return player
+    else:    
+        return False
+
+async def create_player(user_id: int, name: str):
+    player = await get_player(user_id)
+    if not player:
+        await db.player.insert_one({"player": user_id, "name": name, "level": 1, "hp": 10, "cur_hp": 10, "money": 50, "attack": 1, "magic_attack=": 0, "exp": 0, "armour": 0, "magic_armour": 0, "location_id": 1})
+        return True
+    else:    
+        return False
+
+async def delete_player(user_id: int):
+    player = await get_player(user_id)
+    if player:
+        await db.player.delete_one({"player": user_id})
+        return True
+    else:    
+        return False
