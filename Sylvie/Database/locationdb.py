7b18d7daa94ffdade98cd5b53487bb36e93f9a33
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-23 20:41:15 +0530

    Delete Sylvie directory

--- Sylvie/Database/locationdb.py
+++
@@ -1,9 +0,0 @@
-from Sylvie import *
-from Sylvie.Database import *
-
-def get_location(loc_id: int):
-    location = locations.get(loc_id)
-    if location:
-        return location
-    else:
-        return False
