3e175c16b609299b5c7d5377eed82a5629b6777a
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 11:56:49 +0530

    Update shop.py

--- Sylvie/plugins/shop.py
+++ Sylvie/plugins/shop.py
@@ -54,33 +54,6 @@ async def show_items_by_type(client: Client, cq: CallbackQuery):
     else:
         await cq.answer("This Wasn't Requested By You")
 
-@app.on_message(filters.command("shop"))
-async def shop(client: Client, message: Message):
-    close_shop = InlineKeyboardButton("Close 🚫", callback_data=f"close_{message.from_user.id}")
-    cur_loc = await get_player(messages.from_user.id)['location_id']
-    y = {}
-    for x in items.values():
-        if not x.get("item_type") in y:
-            y[x.get("item_type")] = x.get("item_symbol")
-        else:
-            pass
-
-    type_buttons = [
-        InlineKeyboardButton(
-            f"Buy {item_type[0].capitalize()}{ 's' if item_type[0][-1]!='s' else ''} {item_type[1]}",
-            callback_data=f"show_{message.from_user.id}_{item_type[0]}_{cur_loc}"
-        ) for item_type in y.items()
-    ]
-
-    buttons = [type_buttons[i:i+2] for i in range(0, len(type_buttons), 2)]
-    markup = InlineKeyboardMarkup([[button for button in row] for row in buttons])
-    markup.inline_keyboard.append([close_shop])
-    await message.reply_text(
-        "🛒 **Welcome To Shop!**", 
-        reply_markup=markup, 
-        parse_mode=enums.ParseMode.MARKDOWN
-          )
-
 @app.on_callback_query(filters.regex("backshop"))
 async def back_shop(client: Client, cq: CallbackQuery):
     user_id = int(cq.data.split("_")[1])
@@ -110,4 +83,31 @@ async def back_shop(client: Client, cq: CallbackQuery):
             "🛒 **Welcome To Shop!**", 
             reply_markup=markup, 
             parse_mode=enums.ParseMode.MARKDOWN
-  )
+        )
+        
+@app.on_message(filters.command("shop"))
+async def shop(client: Client, message: Message):
+    close_shop = InlineKeyboardButton("Close 🚫", callback_data=f"close_{message.from_user.id}")
+    cur_loc = await get_player(messages.from_user.id)['location_id']
+    y = {}
+    for x in items.values():
+        if not x.get("item_type") in y:
+            y[x.get("item_type")] = x.get("item_symbol")
+        else:
+            pass
+
+    type_buttons = [
+        InlineKeyboardButton(
+            f"Buy {item_type[0].capitalize()}{ 's' if item_type[0][-1]!='s' else ''} {item_type[1]}",
+            callback_data=f"show_{message.from_user.id}_{item_type[0]}_{cur_loc}"
+        ) for item_type in y.items()
+    ]
+
+    buttons = [type_buttons[i:i+2] for i in range(0, len(type_buttons), 2)]
+    markup = InlineKeyboardMarkup([[button for button in row] for row in buttons])
+    markup.inline_keyboard.append([close_shop])
+    await message.reply_text(
+        "🛒 **Welcome To Shop!**", 
+        reply_markup=markup, 
+        parse_mode=enums.ParseMode.MARKDOWN
+          )
