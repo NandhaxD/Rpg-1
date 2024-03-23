38b5e93141f2380adaea44a7684cf966c2fd6545
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 20:48:19 +0530

    Update __init__.py

--- Aasf/__init__.py
+++ Aasf/__init__.py
@@ -8,7 +8,7 @@ app = Client(
     "Aasf",
     "14676558",
     "b3c4bc0ba6a4fc123f4d748a8cc39981",
-    "6690815586:AAFruivCyJHGEzTwR5ayqkxvaN2TXNpcgFI"
+    "6690815586:AAFdkuqgTo2EHlkXlid9HBC2hnsW0flVsDU"
 )
 app.start()
 async_mongo_client = async_mongo("mongodb+srv://Aasf:Aasf@aasf.ere7cca.mongodb.net/?retryWrites=true&w=majority&appName=Aasf")
