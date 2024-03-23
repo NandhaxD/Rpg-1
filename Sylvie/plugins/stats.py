ec5c707d10acab025ade0f9c5fef9b5285284fb0
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 11:52:24 +0530

    Update stats.py

--- Sylvie/plugins/stats.py
+++ Sylvie/plugins/stats.py
@@ -4,9 +4,10 @@ from pyrogram.types import *
 from Sylvie import *
 from Sylvie.Database import *
 
-@app.on_callback_query(filters.regex("stats"))
-async def stats_c(client: Client, cq: CallbackQuery):
-    player = await get_player(cq.from_user.id)
+@app.on_message(filters.command("stats"))
+async def stats(_, message):
+    inv_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Inventory 💼", callback_data=f"inventory_{message.from_user.id}")]])
+    player = await get_player(message.from_user.id)
     name = player['name']
     level = player['level'] 
     hp = player['hp'] 
@@ -17,39 +18,38 @@ async def stats_c(client: Client, cq: CallbackQuery):
     m_armour = player['magic_armour']
     exp = player['exp']
     balance = player['money']
-    await cq.edit_message_text(f"🧝 **Your Character""s Statistics:**\n\n"
-                                    f"**name:** `{name}`\n\n"
-                                    f"**level:** `{level}` (`{100 - exp}` **To Sl.**)\n\n"
-                                    f"**Health:** `{cur_hp}`/`{hp}`\n\n"
-                                    f"**Damage:** `{attack}` ⚔️  `{m_attack}` 🪄\n\n"
-                                    f"**Armor:** `{armour}` 🛡️  `{m_armour}` 🔮\n\n"
-                                    f"**Balance:** `{balance}` 💎", reply_markup=back_markup,
-                            parse_mode=enums.ParseMode.MARKDOWN)
-    
-@app.on_message(filters.command("stats"))
-async def stats(_, message):
-    player = await get_player(message.from_user.id)
-    if not player:
-        if message.chat.type == enums.ChatType.PRIVATE:
-            return await message.reply_text("`You've Not Registered Yet Use /start To Get Register`")
-        else:
-            return await message.reply_text("`You've Not Registered Yet Use /start To Get Register`\n\n`Click The Below Button To Get Registered`", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="hi", url="https://telegram.me/AracadianStarBot?start=True")]]))
-    else:   
+    stats_text = "**👤 Your Character's Statistics:**\n\n"
+    stats_text += f"**Name:** `{name}`\n\n"
+    stats_text += f"**Level:** `{level}` (`{level*1000 - exp}` **To Next Level**)\n\n"
+    stats_text += f"**Health:** `{cur_hp}`/`{hp}`\n\n"
+    stats_text += f"**Attack:** `{attack}` ⚔️  `{m_attack}` 🪄\n\n"
+    stats_text += f"**Defense:** `{armour}` 🛡️  `{m_armour}` 🔮\n\n"
+    stats_text += f"**Balance:** `{balance}` 💎\n\n"
+    await message.reply_text(stats_text, reply_markup=inv_markup, parse_mode=enums.ParseMode.MARKDOWN)
+
+@app.on_callback_query(filters.regex("stats"))
+async def stats(_, cq):
+    user_id = int(cq.data.split("_")[1])
+    if not cq.from_user.id == user_id:
+        return await cq.answer("This Wasn't Requested By You")
+    else:
+        inv_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Inventory 💼", callback_data=f"inventory_{cq.from_user.id}")]])
+        player = await get_player(cq.from_user.id)
         name = player['name']
         level = player['level'] 
         hp = player['hp'] 
         cur_hp = player['cur_hp']
         attack = player['attack']
-        magic_attack = player['magic_attack']
+        m_attack = player['magic_attack']
         armour = player['armour']
-        magic_armour = player['magic_armour']
-        exp = player['exp']
+        m_armour = player['magic_armour']
+        xp = player['xp']
         balance = player['money']
-        stats_text = "👤 **Your Character's Statistics:**\n\n"
-        stats_text += f"**Name:** `{name}`\n"
-        stats_text += f"**Level:** `{level}` (`{level*10 - exp}` **To Next Level**)\n"
-        stats_text += f"**Health:** `{cur_hp}`/`{hp}`\n"
-        stats_text += f"**Attack:** `{attack}` ⚔️  `{magic_attack}` 🪄\n"
-        stats_text += f"**Defense:** `{armour}` 🛡️  `{magic_armour}` 🔮\n"
-        stats_text += f"**Balance:** `{balance}` 💎\n"
-        await message.reply_text(stats_text, parse_mode=enums.ParseMode.MARKDOWN)
+        stats_text = "**👤 Your Character's Statistics:**\n\n"
+        stats_text += f"**Name:** `{name}`\n\n"
+        stats_text += f"**Level:** `{level}` (`{level*10 - exp}` **To Next Level**)\n\n"
+        stats_text += f"**Health:** `{cur_hp}`/`{hp}`\n\n"
+        stats_text += f"**Attack:** `{attack}` ⚔️  `{m_attack}` 🪄\n\n"
+        stats_text += f"**Defense:** `{armour}` 🛡️  `{m_armour}` 🔮\n\n"
+        stats_text += f"**Balance:** `{balance}` 💎\n\n"
+        await cq.edit_message_text(stats_text, reply_markup=inv_markup, parse_mode=enums.ParseMode.MARKDOWN)
