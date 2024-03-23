94e1892db8e7be7d314d0f4d389205302b9da8dc
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2024-03-23 16:41:14 +0530

    Update callback.py

--- Sylvie/plugins/callback.py
+++ Sylvie/plugins/callback.py
@@ -1,34 +1,40 @@
     elif cq.data == 'attack':
-        cur_fights[cq.from_user.id][3].answered = True
-        enemy = cur_fights[cq.from_user.id][0]
-        player = cur_fights[cq.from_user.id][1]
+        await update_time(cq.from_user.id)
+        battle = get_battle(cq.from_user.id)
+        enemy = battle["enemy"]
+        player = await get_player(cq.from_user.id)
         damage = player['attack'] * 1.5 if random.random() < 0.2 else player['attack']
         enemy['hp'] -= max(damage - enemy['armour'], 0)
-        crit = f"**attack On** `{damage}` **Damage!**"
+        crit = f"**Attack On** `{damage}` **Damage!**"
+        win_markup = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton("Back", callback_data="leave_city")],
+                [InlineKeyboardButton("Keep Going", callback_data="enter_dungeon")]
+            ])
         if damage > player['attack']:
-            crit = f"**Critical attack On** `{damage}` **Damage!**"
+            crit = f"**Critical Attack On** `{damage}` **Damage!**"
         if enemy['hp'] <= 0:
             lup = ""
-            new_xp = player['xp'] + enemy['xp']
+            new_xp = player['exp'] + enemy['exp']
             if new_xp >= 100:
                 player['level'] += 1
                 player['hp'] += 10
-                player['xp'] = new_xp - 100
-                lup = f"\n\n**level Up! Now Your level:** `{player['level']}`"
+                player['exp'] = new_xp - 100
+                lup = f"\n\n**Level Up! Now Your Level:** `{player['level']}`"
             else:
-                player['xp'] = new_xp
+                player['exp'] = new_xp
             player['money'] += enemy['money']
-            await cq.edit_message_text(f"{crit}\n\n**You Won! Received** `{enemy['xp']}` **Experience And** `{enemy['money']}` **Coins.**"
+            await cq.edit_message_text(f"{crit}\n\n**You Won! Received** `{enemy['exp']}` **Experience And** `{enemy['money']}` **Coins.**"
                                              f"{lup}",
                                         reply_markup=win_markup, parse_mode=enums.ParseMode.MARKDOWN)
-            player['xp'] += enemy['xp']
+            player['exp'] += enemy['exp']
             player['money'] += enemy['money']
             player['location_id'] = cur_fights[player['name']][2]
             cur_fights.pop(player['name'])
         else:
             for i in range(0, 4):
                 await cq.edit_message_text(f"{crit}\n**The Enemy Has** `{enemy['hp']}` **Health.**\n\n"
-                                                 f"`The Enemy Is attacking`" + "." * (i % 4), parse_mode=enums.ParseMode.Markdown)
+                                                 f"`The Enemy Is Attacking`" + "." * (i % 4), parse_mode=enums.ParseMode.Markdown)
                 await asyncio.sleep(0.6)
             if enemy['attack_type'] == 'phys':
                 enemy_damage = random.choices([enemy['attack'], enemy['attack'] * 1.5], weights=[0.8, 0.2])[0]
