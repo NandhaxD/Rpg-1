47588ec486fb0a9e7228ae2a377c69c43d581e8d
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-24 06:07:19 +0530

    Update __main__.py

--- Aasf/__main__.py
+++ Aasf/__main__.py
@@ -1,6 +1,4 @@
 import asyncio
-import logging
-import importlib
 from pyrogram import *
 
 from Aasf import app
@@ -9,7 +7,12 @@ from Aasf.plugins import *
 
 FORMAT = "%(message)s"
 
-async def main():
+async def check_inactive_users_loop():
+    while True:
+        await check_inactive_users(app)
+        await asyncio.sleep(0.1)
+
+if __name__ == "__main__":
     logging.basicConfig(
         handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
         level=logging.DEBUG,
@@ -18,11 +21,8 @@ async def main():
     )
     logging.getLogger("pyrogram").setLevel(logging.INFO)
     for module in ALL_MODULES:
-      importlib.import_module("Aasf.plugins." + module)
-    await idle()
+        importlib.import_module("Aasf.plugins." + module)
+    idle()
+    asyncio.create_task(check_inactive_users_loop())
     while True:
-        await check_inactive_users(app)
-        await asyncio.sleep(0.1)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        await asyncio.sleep(1)
