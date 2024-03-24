6490f38ebb5b3cf49359056bf6904ebb2ccf25a4
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-24 06:20:02 +0530

    Update __main__.py

--- Aasf/__main__.py
+++ Aasf/__main__.py
@@ -11,6 +11,12 @@ from Aasf.plugins import *
 FORMAT = "%(message)s"
 
 async def main():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_inactive_users, "interval", seconds=0.1, args=[app])
+    scheduler.start()
+    await asyncio.get_event_loop().run_forever()
+
+if __name__ == "__main__":
     logging.basicConfig(
         handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
         level=logging.DEBUG,
@@ -20,11 +26,5 @@ async def main():
     logging.getLogger("pyrogram").setLevel(logging.INFO)
     for module in ALL_MODULES:
       importlib.import_module("Aasf.plugins." + module)
-    await idle()
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(check_inactive_users, "interval", seconds=0.1, args=[app])
-    scheduler.start()
-    await asyncio.get_event_loop().run_forever()
-
-if __name__ == "__main__":
+    idle()
     asyncio.run(main())
