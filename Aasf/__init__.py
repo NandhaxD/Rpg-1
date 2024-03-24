aeb425b01e11653e48e2d1c9d2dfad08cd7bdbab
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-24 12:16:03 +0530

    Update __init__.py

--- Aasf/__init__.py
+++ Aasf/__init__.py
@@ -7,8 +7,9 @@ app = Client(
     name="Aasf",
     api_id="14676558",
     api_hash="b3c4bc0ba6a4fc123f4d748a8cc39981",
-    bot_token="6690815586:AAFdkuqgTo2EHlkXlid9HBC2hnsW0flVsDU"
+    bot_token="5617659434:AAG5MSWbbdUyymx2ZcQLCphIWwaki8ewiu8"
 )
+DEVS = [5456798232]
 app.start()
 async_mongo_client = async_mongo("mongodb+srv://jinn1:jinn1@cluster0.iug55ex.mongodb.net/?retryWrites=true&w=majority")
 db = async_mongo_client.Aasf
