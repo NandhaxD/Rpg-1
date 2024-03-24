39a269df9b1c38e9f46be03885225b02e8a04846
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-24 05:58:56 +0530

    Update __init__.py

--- Aasf/__init__.py
+++ Aasf/__init__.py
@@ -3,12 +3,11 @@ import pyromod
 from pyrogram import *
 from motor.motor_asyncio import AsyncIOMotorClient as async_mongo
 
-
 app = Client(
     name="Aasf",
     api_id="14676558",
     api_hash="b3c4bc0ba6a4fc123f4d748a8cc39981",
-    bot_token="5617659434:AAFPGDeIl7JNHEHxx3t0s1DBzt2rQvc0DpY"
+    bot_token="6690815586:AAFdkuqgTo2EHlkXlid9HBC2hnsW0flVsDU"
 )
 app.start()
 async_mongo_client = async_mongo("mongodb+srv://Aasf:Aasf@aasf.ere7cca.mongodb.net/?retryWrites=true&w=majority&appName=Aasf")
