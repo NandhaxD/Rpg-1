fb40e74409a420924f9efc2b5b341c3bdb21b93b
Flora 흥분한 <106656839+HornyFlora@users.noreply.github.com>
2024-03-24 12:19:06 +0530

    Update chatwatcher.py

--- Aasf/plugins/chatwatcher.py
+++ Aasf/plugins/chatwatcher.py
@@ -6,10 +6,8 @@ from Aasf.Database import *
 async def chat_watcher_func(_, message):
     if message.from_user:
         us_in_db = await get_user(message.from_user.id)
-        us_in_chat = await get_user_chat(message.chat.id, message.from_user.id)
-        if not us_in_db or not us_in_chat:
+        if not us_in_db:
             await add_user(message.from_user.id)
-            await add_user_chat(message.chat.id, message.from_user.id)
 
     chat_id = (message.chat.id if message.chat.id != message.from_user.id else None)
 
@@ -21,4 +19,3 @@ async def chat_watcher_func(_, message):
     in_db = await get_chat(chat_id)
     if not in_db:
         await add_chat(chat_id)
-        await db.count.insert_one({"chat_id": chat_id, "count": 0})
